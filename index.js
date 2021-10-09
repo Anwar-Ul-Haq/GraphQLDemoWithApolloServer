5d24006cc8d86217943043d0f441f30f41adcaa5
Anwar Ul Haq <aulhaq6@gmail.com>
2021-10-09 17:32:08 +0100

    added mutations

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
 
+   type ProgrammingDay{
+       id: ID!
+       date:String!
+       language:String!
+       conditions: Conditions
+           
+   }
+
+   enum Conditions{
+       IF
+       SWITCH
+       FOR
+       IFELSE
+   }
+
+
  type Query{
   totalDays:Int!
+  allDays:[ProgrammingDay!]!
 
 
+ }
+
+ input AddDayInput {
+     date: String!
+     conditions: Conditions
+ }
+
+ type Mutation{
+    addDay(input:AddDayInput) : ProgrammingDay
+    removeDay(id:ID!):ProgrammingDay
  }
    
 
