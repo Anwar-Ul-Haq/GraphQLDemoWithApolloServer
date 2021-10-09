f231b4bb16ea0c621cf22d91ca8e08f1986dc3d2
Anwar Ul Haq <aulhaq6@gmail.com>
2021-10-09 19:26:11 +0100

    scalar type and subscription for  GraphQL

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql , MockList} = require("apollo-server");
 
 const typeDefs = gql`
 
+    scalar Date
+    
+    """
+     An Object that describes the characteristics of a programing day
+    """
    type ProgrammingDay{
+       "Unique identifier"
        id: ID!
-       date:String!
+       date:Date!
+       "Programming language"
        language:String!
        conditions: Conditions
            
@@ -26,17 +33,36 @@ const typeDefs = gql`
  }
 
  input AddDayInput {
-     date: String!
+     date: Date!
      conditions: Conditions
+ }
+
+ type RemoveDayPayLoad{
+     day:ProgrammingDay!
+     remove:Boolean
+     totalBefore:Int
+     totalAfter:Int
+
+
  }
 
  type Mutation{
     addDay(input:AddDayInput) : ProgrammingDay
-    removeDay(id:ID!):ProgrammingDay
+    removeDay(id:ID!):RemoveDayPayLoad!
  }
    
+type Subscription{
+    newDay:ProgrammingDay!
 
+
+}
 `;
+
+const mocks = {
+    Date: () => "1/2/2025",
+    String: () => "Cool Data"
+    
+}
 
 // const resolvers = {
 
@@ -48,7 +74,7 @@ const server = new ApolloServer({
     typeDefs,
    // resolvers
     
-    mocks:true
+    mocks
 })
 
 server.listen().then(({ url }) => console.log(`Server running  at ${url}`));
